bd1d350a07ea05a4d0f7ddeb22a17b3f9c20b55c
Danny Ben Shitrit <dannyb@telesofia.com>
2018-04-20 15:11:09 +0000

    reduce cognitive complexity

--- lib/webcache/response.rb
+++ lib/webcache/response.rb
@@ -4,18 +4,28 @@ class WebCache
 
     def initialize(opts={})
       if opts.respond_to?(:read) && opts.respond_to?(:base_uri)
-        self.content  = opts.read
-        self.base_uri = opts.base_uri
-        self.error    = nil
+        init_with_uri opts
       elsif opts.is_a? Hash
-        self.error    = opts[:error]    if opts[:error]
-        self.base_uri = opts[:base_uri] if opts[:base_uri]
-        self.content  = opts[:content]  if opts[:content]
+        init_with_hash opts
       end
     end
 
     def to_s
       content
+    end
+
+    private
+
+    def init_with_uri(opts)
+      self.content  = opts.read
+      self.base_uri = opts.base_uri
+      self.error    = nil
+    end
+
+    def init_with_hash(opts)
+      self.error    = opts[:error]
+      self.base_uri = opts[:base_uri]
+      self.content  = opts[:content]
     end
   end
 end
